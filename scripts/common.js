634fd2fca3bbaf4fb9bcc53df18146a2c61693cc
kimnani01 <165613415+kimnani01@users.noreply.github.com>
2024-07-24 12:16:02 +0900

    header 메뉴 수정, list 페이지 수정

--- scripts/common.js
+++ scripts/common.js
@@ -3,11 +3,16 @@ const category_btn = document.querySelectorAll('.gnb > li > a')
 const sub_bg = document.querySelector('li > .bg')
 console.log(category_btn[0])
 console.log(sub_bg)
+let openclose = false; //접힘상태 초기
+// 점힘(false)*기본 열림(true)
 category_btn[0].addEventListener('click', (e)=>{
     e.preventDefault()
-    if(sub_bg.style.display == 'none'){
+    if(openclose == false){
         sub_bg.style.display = 'block'
+        // openclose = true;
     }else{
         sub_bg.style.display = 'none'
+        // openclose = false;
     }
+    openclose = !openclose
 })
