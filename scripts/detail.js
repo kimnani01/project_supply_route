088ea9f771c50698f9fb15d7a8bae5a2bd830d64
kimnani01 <165613415+kimnani01@users.noreply.github.com>
2024-07-25 20:59:18 +0900

    detail 탭버튼 누르면 해당위치 이동 js

--- scripts/detail.js
+++ scripts/detail.js
@@ -34,32 +34,51 @@ more_btn.addEventListener('click',(e)=>{
 
 // 탭버튼 클릭 활성화
 const tab_btn = document.querySelectorAll('.tab li a')
-for(let i of tab_btn){
-    i.addEventListener('click',()=>{
-        for(let j of tab_btn){
-            j.parentElement.classList.remove('active')
-            j.parentElement.style.border = '1px solid #bbb'
-            j.parentElement.style.borderBottom = '2px solid #000'
-        }
-        i.parentElement.style.border = '2px solid #000'
-        i.parentElement.style.borderBottom = '#fff'
-        console.log(i.parentElement.previousElementSibling)
-        i.parentElement.previousElementSibling.style.borderRight = '0'
-    })
-}
-// 탭버튼 위치 활성화
 const tab_pst = document.querySelectorAll('.tab_pst')
-// i.getBoundingClientRect().top + 207
+// for(let i of tab_btn){
+//     i.addEventListener('click',()=>{
+//         window.scrollTo(0, i.getBoundingClientRect().top + 207)
+//         for(let j of tab_btn){
+//             j.parentElement.classList.remove('active')
+//             j.parentElement.style.border = '1px solid #bbb'
+//             j.parentElement.style.borderBottom = '2px solid #000'
+//         }
+//         i.parentElement.style.border = '2px solid #000'
+//         i.parentElement.style.borderBottom = '#fff'
+//         // console.log(i.parentElement.previousElementSibling)
+//         // i.parentElement.previousElementSibling.style.borderRight = '0'
+//     })
+// }
+const review = document.querySelector('.review')
+tab_btn.forEach((target, index)=>{
+    console.log(target, index)
+    target.addEventListener('click',(e)=>{
+        // e.preventDefault()
+        console.log(tab_pst[index].getBoundingClientRect().top + 237)
+        scrollTo(0, review.getBoundingClientRect().top + 237)
+        for(let i of tab_btn){
+            i.parentElement.classList.remove('active')
+            i.parentElement.style.border = '1px solid #bbb'
+            i.parentElement.style.borderBottom = '2px solid #000'
+        }
+        target.parentElement.style.border = '2px solid #000'
+        target.parentElement.style.borderBottom = '#fff'
+        // console.log(i.parentElement.previousElementSibling)
+        // i.parentElement.previousElementSibling.style.borderRight = '0'
+    })
+})
+// 탭버튼 위치 활성화
+// i.getBoundingClientRect().top + 148+59+30=237
 window.addEventListener('scroll',()=>{
     // 1. window scroll 인식
     // console.log(window.pageYOffset)
     // 2. 요소 하나 기준
     // console.log(tab_pst[0].getBoundingClientRect().top)
-    // 3. 12 비교 if
+    // for each
     tab_pst.forEach((target, index)=>{
+        // 3. 1,2 비교 if
         if(target.getBoundingClientRect().top < 250){
             // console.log('같다')
-            // for each
             //tab_btn[0].parentElement.style.border = '2px solid #000'
             //tab_btn[0].parentElement.style.borderBottom = '#fff'
             for(let i of tab_btn){
@@ -121,8 +140,10 @@ size_guide_btn.addEventListener('click',(e)=>{
     e.preventDefault()
     if(drop_size == false){
         size_guide_btn.nextElementSibling.style.display = 'block'
+        size_guide_btn.lastElementChild.src = "./images/detail/minus_btn.png"
     }else{
         size_guide_btn.nextElementSibling.style.display = 'none'
+        size_guide_btn.lastElementChild.src = "./images/detail/plus_btn.png"
     }
     drop_size = !drop_size
 })
@@ -134,8 +155,10 @@ brief_btn.addEventListener('click',(e)=>{
     e.preventDefault()
     if(drop_brief == false){
         brief_btn.nextElementSibling.style.display = 'block'
+        brief_btn.lastElementChild.src = "./images/detail/minus_btn.png"
     }else{
         brief_btn.nextElementSibling.style.display = 'none'
+        brief_btn.lastElementChild.src = "./images/detail/plus_btn.png"
     }
     drop_brief = !drop_brief
 })
